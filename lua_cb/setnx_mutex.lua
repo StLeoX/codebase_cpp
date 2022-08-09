43695a85555af65f2999079af2cf2b1317f7ffff
leolaxie <leolaxie@tencent.com>
2022-08-09 11:21:20 +0800

    add lua_cb

---
+++ lua_cb/setnx_mutex.lua
@@ -0,0 +1,46 @@
+-- 基于setnx指令的分布式互斥锁
+
+-- lock01
+-- args:
+-- KEYS[1] 锁键
+-- ARGV[1] 锁值
+-- ARGV[2] 持锁时间
+-- RET 1==succ 0=fail
+if redis.call('SETNX', KEYS[1], ARGV[1])
+then
+    if redis.call('GET', KEYS[1]) == ARGV[1]
+    then
+        return redis.call('EXPIRE', KEYS[1], ARGV[2])
+    else
+        return 0
+    end
+else
+    return 0
+end
+
+
+-- lock02
+-- args:
+-- KEYS[1] 锁键
+-- ARGV[1] 锁值
+-- ARGV[2] 持锁时间
+-- RET 1==succ 0=fail
+-- pattern: set "$key" "$value" NX EX $time
+if redis.call('SETNX', KEYS[1], ARGV[1], 'NX', 'EX', ARGV[2])
+then
+    return redis.call('GET', KEYS[1]) == ARGV[1]
+else
+    return 0
+end
+
+-- unlock
+-- args:
+-- KEYS[1] 锁键
+-- ARGV[1] 期望锁值（期望未修改）
+-- RET 1==succ 0=fail
+if redis.call('GET', KEYS[1]) == ARGV[1]
+then
+    return redis.call('DEL', KEYS[1]) -- 并清除
+else
+    return 0
+end
